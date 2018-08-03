1c1341084e183427a6c5a7ceb65e54e9203d17f4
Rowell Heria <rowell.heria@answerdigital.com>
2018-08-03 14:31:41 +0100

    added new fields to Actor dto

--- src/main/java/com/answerdigital/appointment/dto/Actor.java
+++ src/main/java/com/answerdigital/appointment/dto/Actor.java
@@ -1,11 +1,19 @@
 package com.answerdigital.appointment.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Actor {
 
 	private String reference;
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private Identifier[] identifier;
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private String display;
+
 	private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
 	public String getReference() {
@@ -13,6 +21,24 @@ public class Actor {
 	}
 	public void setReference(String reference) {
 		this.reference = reference;
+	}
+
+	public Identifier[] getIdentifier()
+	{
+		return identifier;
+	}
+	public void setIdentifier(Identifier[] identifier)
+	{
+		this.identifier = identifier;
+	}
+
+	public String getDisplay()
+	{
+		return display;
+	}
+	public void setDisplay(String display)
+	{
+		this.display = display;
 	}
 	
 	public Map<String, Object> getAdditionalProperties() {
